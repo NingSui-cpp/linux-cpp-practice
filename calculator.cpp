8cbfddd7c4db0a3e39b27897b3705f216e0716d1
Ning <suin@tcd.ie>
2026-05-28 05:19:44 +0000

    finish basic cpp practice

---
+++ calculator.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	double a, b;
+	char op;
+	cin >> a >> op >> b;
+	switch (op){
+		case '+':
+			cout << a + b << endl;
+			break;
+		case '-':
+			cout << a - b << endl;
+			break;
+		case '*':
+			cout << a * b << endl;
+			break;
+		case '/':
+			cout << a / b << endl;
+			break;
+		default:
+			cout << "Invalid operator" << endl;
+	}
+	return 0;
+}
