69787be77f8c01dcc33f6bf54b9cd73fe9799645
Ning <suin@tcd.ie>
2026-05-29 03:28:54 +0000

    Add power, sqrt and modulo operations

--- calculator.cpp
+++ calculator.cpp
@@ -1,26 +1,58 @@
-
 #include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
 
 int main(){
 	double a, b;
 	char op;
-	cin >> a >> op >> b;
-	switch (op){
-		case '+':
-			cout << a + b << endl;
+	int power;
+	string first;
+	cout << "The symbol for a square is ^2" << endl << "The symbol for a square root is r" << endl;
+	while(true){
+		cin >> a >> op;
+		if(op == 'r'){
+			cout << sqrt(a) << endl;
+		}
+		else{
+			cin >> b;
+			switch (op){
+				case '+':
+					cout << a + b << endl;
+					break;
+				case '-':
+					cout << a - b << endl;
+					break;	
+				case '*':
+					cout << a * b << endl;
+					break;
+				case '/':
+					if(b == 0){
+						cout << "The denominator cannot be zero" << endl;
+					}
+					else{
+						cout << a / b << endl;
+					}
+					break;
+				case '%':
+					cout <<(int) a %(int) b << endl;
+					break;
+				case '^':
+					cout << pow(a,b) << endl;
+					break;
+				case 'r':
+					cout << sqrt (a) << endl;
+					break;
+				default:
+					cout << "Invalid operator" << endl;
+			}
+		}
+		cout << "Do you want to quit or continue? type quit if you want to quit, type continue if you want to continue." 
+			<< endl;
+		cin >> first;
+		if(first == "quit"){
 			break;
-		case '-':
-			cout << a - b << endl;
-			break;
-		case '*':
-			cout << a * b << endl;
-			break;
-		case '/':
-			cout << a / b << endl;
-			break;
-		default:
-			cout << "Invalid operator" << endl;
+		}
 	}
 	return 0;
 }
